390ed4508b9c59e392b7d24b998e416d75103e26
Dhruv Aggarwal <dhruv.2002agg@gmail.com>
2023-07-19 19:28:29 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-PQT3Qmih2bdbJOx27s2XW8kc",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a Car Ginie. You can help people choose waht car they want to buy.",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
